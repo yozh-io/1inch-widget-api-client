247d440ffefe0c96ee1f56b837f706e6f5e4bc71
Andrii Leonets <awdenheimlee@gmail.com>
2022-07-20 11:32:40 -0700

    Add networks (#5)
    * feat: add networks
    
    * feat: update exports

---
+++ client/index.js
@@ -0,0 +1,20 @@
+"use strict";
+const arbitrumApi = require('./networks/arbitrum/dist');
+const avalancheApi = require('./networks/avalanche/dist');
+const binanceApi = require('./networks/binance/dist');
+const ethereumApi = require('./networks/ethereum/dist');
+const fantomApi = require('./networks/fantom/dist');
+const gnosisApi = require('./networks/gnosis/dist');
+const optimismApi = require('./networks/optimism/dist');
+const polygonApi = require('./networks/polygon/dist');
+
+module.exports = {
+  arbitrumApi: arbitrumApi,
+  avalancheApi: avalancheApi,
+  binanceApi: binanceApi,
+  ethereumApi: ethereumApi,
+  fantomApi: fantomApi,
+  gnosisApi: gnosisApi,
+  optimismApi: optimismApi,
+  polygonApi: polygonApi,
+};
