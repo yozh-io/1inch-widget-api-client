e47af83316c474a91bf49596c9d2520ee0f46aa7
Andrii Leonets <awdenheimlee@gmail.com>
2022-07-21 02:38:46 -0700

    Add dists (#6)
    * feat: add networks
    
    * feat: update exports
    
    * feat: add dists

---
+++ client/networks/fantom/dist/models/index.js
@@ -0,0 +1,19 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+/* eslint-disable */
+__export(require("./ApproveCalldataResponseDto"));
+__export(require("./ApproveSpenderResponseDto"));
+__export(require("./NestErrorMeta"));
+__export(require("./PathViewDto"));
+__export(require("./ProtocolImageDto"));
+__export(require("./ProtocolsResponseDto"));
+__export(require("./QuoteResponseDto"));
+__export(require("./SwapErrorDto"));
+__export(require("./SwapResponseDto"));
+__export(require("./TokenDto"));
+__export(require("./TokensResponseDto"));
+__export(require("./Tx"));
