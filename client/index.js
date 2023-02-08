0bec6c5b17918f397cd97963e76ee48434a4421d
trandobulo <trandobulo@gmail.com>
2023-02-08 13:42:43 +0200

    feat: add aurora & klaytn networks api

--- client/index.js
+++ client/index.js
@@ -1,12 +1,14 @@
 "use strict";
-const arbitrumApi = require('./networks/arbitrum/dist');
-const avalancheApi = require('./networks/avalanche/dist');
-const binanceApi = require('./networks/binance/dist');
-const ethereumApi = require('./networks/ethereum/dist');
-const fantomApi = require('./networks/fantom/dist');
-const gnosisApi = require('./networks/gnosis/dist');
-const optimismApi = require('./networks/optimism/dist');
-const polygonApi = require('./networks/polygon/dist');
+const arbitrumApi = require("./networks/arbitrum/dist");
+const avalancheApi = require("./networks/avalanche/dist");
+const binanceApi = require("./networks/binance/dist");
+const ethereumApi = require("./networks/ethereum/dist");
+const fantomApi = require("./networks/fantom/dist");
+const gnosisApi = require("./networks/gnosis/dist");
+const optimismApi = require("./networks/optimism/dist");
+const polygonApi = require("./networks/polygon/dist");
+const auroraApi = require("./networks/aurora/dist");
+const klaytnApi = require("./networks/klaytn/dist");
 
 module.exports = {
   arbitrumApi: arbitrumApi,
@@ -17,4 +19,6 @@ module.exports = {
   gnosisApi: gnosisApi,
   optimismApi: optimismApi,
   polygonApi: polygonApi,
+  auroraApi: auroraApi,
+  klaytnApi: klaytnApi,
 };
